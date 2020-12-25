6b97fda81c1b5d719bb3f2c8b769b5f3c759be7f
tuboihiroki <tuboi.hiroki@icloud.com>
2020-12-25 12:28:56 +0900

    Creating the Recorder Component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Recorder from '../Recorder/Recorder';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Recorder />
+    </div>
+  );
 }
 
 export default App;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from 'redux';
+import recorderReducer from './recorder';
 import userEventsReducer from './user-event';
 
 const rootReducer = combineReducers({
   uerEvents: userEventsReducer,
+  recorder: recorderReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 
